ccd3ec271452758cb41ec29798aff4d341cf55b1
Mandelstein <vantoshechkin98@mail.ru>
2017-11-07 22:25:59 +0300

    updated views,urls,templates

--- book/urls.py
+++ book/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.names, name='names'),
+    url(r'^books', views.book_list, name='book_list'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Book
+def book_list(request):
+    books=Book.objects.order_by('title')
+    return render(request,'book/book_list.html',{'books':books})
+def book_show(request,book_id):
+    book=Book.objects.get(id=book_id)
+    return render()
+def remove_book(request,book_id):
+    obj=Book.objects.get(id=book_id)
+    obj.delete
 
-# Create your views here.
 def names(request):
     return render(request, 'book/base.html', {})
